c3bc5cae6749cfbcfb6121a5ee8e4fdc3f614ac0
Bartek <bartekwieteckiyt@gmail.com>
2024-02-08 22:32:55 +0100

    added possibility to display name of the winner

--- src/App.jsx
+++ src/App.jsx
@@ -24,8 +24,8 @@ const initialGameBoard = [
 
 function App() {
   const [players, setPlayers] = useState({
-    'X': 'Player 1',
-    'O': 'Player 2',
+    X: 'Player 1',
+    O: 'Player 2',
   });
   const [gameTurns, setGameTurns] = useState([]);
   // const [hasWinnerm, setHasWinner] = useState(false);
@@ -53,7 +53,7 @@ function App() {
       firstSquareSymbol === secondSquareSymbol &&
       firstSquareSymbol === thirdSquareSymbol
     ) {
-      winner = firstSquareSymbol;
+      winner = players[firstSquareSymbol];
     }
   }
 
@@ -94,11 +94,13 @@ function App() {
             initialName="Player 1"
             symbol="X"
             isActive={activePlayer === 'X'}
+            onChangeName={handlePlayerNameChange}
           />
           <PlayerInfo
             initialName="Player 2"
             symbol="O"
             isActive={activePlayer === 'O'}
+            onChangeName={handlePlayerNameChange}
           />
         </ol>
         {(winner || hasDraw) && (
